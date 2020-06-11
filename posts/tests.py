fce2b22924b296c8a77c88a6ffcaf1b0acf53612
zeyderan <aykutbayraktar34@gmail.com>
2020-06-11 15:16:47 +0300

    testler ekledi

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
-
+from .models import Post
+from django.urls import reverse
 # Create your tests here.
+
+# post text mesajarın doğruluğunu kotrol eder
+class PostModelTest(TestCase):
+    #test ama.lı oluşturulan post
+    #text değeri atanıyor
+    def setUp(self):
+        Post.objects.create(text='deneme text')
+    
+    #asıl test oluşturulan post text ile beklenen post textin eşitliğini kontrol eder
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        beklenen = f'{post.text}'
+        self.assertEqual(beklenen,'deneme text')
+    
+class HomePageViewTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text='test')
+
+    def test_view_url_exists_at_proper_location(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code,200)
+    
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code,200)
+    
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code,200)
+        self.assertTemplateUsed(resp,'home.html')
